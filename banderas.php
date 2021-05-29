32a2192a7d27c0864f6497dfd51e687b1483e62d
Antonio_Aranda7 <L17390318@chetumal.tecnm.mx>
2021-05-29 18:14:38 -0500

    Devuelve una respuesta Json

--- banderas.php
+++ banderas.php
@@ -8,7 +8,7 @@ $parametro3 = $_POST['parametro3'];
 //https://restcountries.eu/rest/v2/region/{region}
 //$response = file_get_contents('https://restcountries.eu/rest/v2/region/'.$region );
 
-$response = file_get_contents('https://restcountries.eu/rest/v2/region/'.$region?fields=.$parametro1;.$parametro2;.$parametro3);
+$response = file_get_contents('https://restcountries.eu/rest/v2/region/'.$region.'?fields='.$parametro1.';'.$parametro2.';'.$parametro3);
 
 //https://restcountries.eu/rest/v2/{service}?fields={field};{field};{field}
 //https://restcountries.eu/rest/v2/region/americas?fields=name;capital;flag
